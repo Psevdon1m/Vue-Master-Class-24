63878bd3a8151f3f4b9b34463e69373379eac7d9
Psevdon1m <Valentine.oleynik@gmail.com>
2025-04-07 15:41:21 +0300

    setup file base routing

---
+++ src/pages/projects/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute<'/projects/[id]'>()
+const projectId = route.params.id
+</script>
+
+<template>
+  <div>
+    <h1>Project ID: {{ projectId }}</h1>
+    <RouterLink to="/projects">Projects</RouterLink>
+  </div>
+</template>
