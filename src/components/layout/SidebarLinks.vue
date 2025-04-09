1236e187abad73d6b95a9a28af1630a4fb41bd17
Psevdon1m <Valentine.oleynik@gmail.com>
2025-04-09 13:25:28 +0300

    refactored router links in sidebar

---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface LinkProp {
+  to: string
+  icon: string
+  title: string
+}
+
+defineProps<{
+  links: LinkProp[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in links"
+    :to="link.to"
+    :key="link.title"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+    :class="{ 'bg-primary/10 text-primary': $route.path === link.to }"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
