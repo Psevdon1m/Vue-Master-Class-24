4578782e54556fad1ba9bfebf03cc8880e411834
Psevdon1m <Valentine.oleynik@gmail.com>
2025-04-14 12:08:49 +0300

    updated vue router for auto-imports

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     VueRouter(),
     AutoImport({
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: './src/auto-imports.d.ts',
       viteOptimizeDeps: true,
