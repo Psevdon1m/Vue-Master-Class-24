8bcba270196932d683561d7396ad1d006452918e
Psevdon1m <Valentine.oleynik@gmail.com>
2025-04-23 18:37:28 +0300

    add quest layout

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,15 @@ onErrorCaptured((error) => {
 onMounted(async () => {
   await authStore.trackAuth()
 })
+
+const { user } = storeToRefs(useAuthStore())
+
+const AuthLayout = defineAsyncComponent(() => import('@/components/Layout/main/AuthLayout.vue'))
+const GuestLayout = defineAsyncComponent(() => import('@/components/Layout/main/GuestLayout.vue'))
 </script>
 
 <template>
-  <AuthLayout>
+  <Component :is="user ? AuthLayout : GuestLayout">
     <AppErrorPage v-if="activeError" />
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
@@ -33,5 +38,5 @@ onMounted(async () => {
         </template>
       </Suspense>
     </RouterView>
-  </AuthLayout>
+  </Component>
 </template>
